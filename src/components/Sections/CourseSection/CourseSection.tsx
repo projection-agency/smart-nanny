fb9d7709522522ce68955f1c7eb43a2674dcb2ca
klimisnotavailable <klimgrigor28@gmail.com>
2025-06-13 12:24:00 +0300

    klim/mobile
    animations

--- src/components/Sections/CourseSection/CourseSection.tsx
+++ src/components/Sections/CourseSection/CourseSection.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Container } from "@/components/Container";
 import s from "./CourseSection.module.css";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const svg = (
   <svg
@@ -36,19 +38,19 @@ const svg = (
   </svg>
 );
 
-// const containerVariants = {
-//   hidden: {},
-//   show: {
-//     transition: {
-//       staggerChildren: 0.18,
-//     },
-//   },
-// };
+const containerVariants = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.18,
+    },
+  },
+};
 
-// const itemVariants = {
-//   hidden: { opacity: 0, y: 30 },
-//   show: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
-// };
+const itemVariants = {
+  hidden: { opacity: 0, y: 30 },
+  show: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
+};
 
 export const CourseSection = () => {
   return (
@@ -62,35 +64,47 @@ export const CourseSection = () => {
         className={s.worm}
       />
       <Container>
-        <h2 className={s.title} >
+        <motion.h2
+          className={s.title}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.9 }}
+          variants={itemVariants}
+        >
           {svg}
           Цей курс
           <span> для вас {line} </span>
           якщо ви...
-        </h2>
+        </motion.h2>
 
-          <ul className={s.list}>
-            {cardsData.map((item, index) => (
-              <li key={index}>
-                <svg>
-                  <use xlinkHref={item.icon}></use>
-                </svg>
+        <ul className={s.list}>
+          {cardsData.map((item, index) => (
+            <motion.li
+              key={index}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.5 }}
+              variants={itemVariants}
+            >
+              <svg>
+                <use xlinkHref={item.icon}></use>
+              </svg>
 
-                <h4>{item.title}</h4>
+              <h4>{item.title}</h4>
 
-                <p>{item.description}</p>
+              <p>{item.description}</p>
 
-                <Image
-                  alt="Border"
-                  width={1920}
-                  height={1080}
-                  priority
-                  src="/images/border-image.png"
-                  className={s.border}
-                />
-              </li>
-            ))}
-          </ul>
+              <Image
+                alt="Border"
+                width={1920}
+                height={1080}
+                priority
+                src="/images/border-image.png"
+                className={s.border}
+              />
+            </motion.li>
+          ))}
+        </ul>
       </Container>
     </section>
   );
@@ -123,12 +137,23 @@ const cardsData = [
 ];
 
 const line = (
-  <svg viewBox="0 0 166 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
+  <motion.svg
+    viewBox="0 0 166 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ width: "100%", height: "auto", verticalAlign: "middle" }}
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <motion.path
       d="M1.5 9.90499C1.66487 8.05717 4.51428 6.82604 5.53649 6.34866C7.80547 5.28904 9.53257 3.60854 11.9865 3.60854C13.4774 3.60854 19.7736 1.7388 16.564 5.35886C15.737 6.29166 15.7361 5.77047 15.309 6.81767C14.724 8.25207 15.7852 7.19703 16.4534 7.08002C19.7508 6.50257 22.5896 6.68791 25.927 6.29166C31.329 5.65028 37.3713 6.92431 42.6993 5.53263C46.6912 4.48993 50.8708 4.57185 54.8437 3.60849C57.476 2.97019 60.131 2.15098 62.8126 2.15098C63.6984 2.15098 65.863 1.41295 65.788 3.20039C65.747 4.17499 63.5696 5.4562 62.9791 5.82391C62.328 6.22932 58.916 7.92273 60.1702 7.92273C65.8032 7.92273 71.0485 6.27314 76.5658 4.7745C81.2947 3.49001 86.0766 4.54438 90.8808 4.54438C96.5121 4.54438 101.881 5.21638 107.547 5.21638C109.702 5.21638 111.479 4.12475 111.479 7.57755C111.479 8.65616 111.399 9.8175 111.082 10.8084M111.082 10.8084C111.063 10.869 111.043 10.9289 111.022 10.9881C110.844 11.4859 109.305 13.7185 109.607 12.7663C109.882 11.8984 110.417 11.2772 111.082 10.8084ZM111.082 10.8084C112.674 9.68732 115.015 9.43808 116.348 8.77271C119.203 7.34814 122.497 6.41483 125.503 5.74108C128.292 5.11601 131.11 4.16697 133.95 4.16697L139.42 4.36445C139.831 4.36445 141.96 4.05179 141.96 4.73888C141.96 5.76298 141.305 6.89213 140.849 7.60955C139.808 9.2466 138.484 10.5981 137.207 11.7283C136.793 12.0938 136.42 12.3932 137.162 12.0473C138.785 11.2914 140.633 11.33 142.312 10.8547C144.351 10.2775 146.259 9.57045 148.186 8.33069C149.774 7.30906 151.429 6.33105 153.106 5.73738C155.444 4.91043 158.022 5.11332 160.408 5.11332C161.745 5.11332 163.194 5.37711 164.5 4.8637"
       stroke="#FFF9C1"
       strokeWidth="3"
       strokeLinecap="round"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{ duration: 1.2, ease: "easeOut" }}
+      viewport={{ once: false, amount: 0.5 }}
     />
-  </svg>
+  </motion.svg>
 );
--- src/components/Sections/TariffSection/TariffSection.tsx
+++ src/components/Sections/TariffSection/TariffSection.tsx
@@ -1,13 +1,14 @@
+"use client";
 import { EducationTariffs } from "@/components/EducationTariffs/EducationTariffs";
 import s from "./TariffSection.module.css";
 import Image from "next/image";
-
+import { motion } from "framer-motion";
 export const TariffSection = () => {
   return (
     <section className={s.section}>
       <h2 className={s.title}>
         {svg}
-        Тарифи на <span>навчання{line}</span>
+        Тарифи на <span>навчання {line}</span>
       </h2>
 
       <EducationTariffs />
@@ -30,101 +31,112 @@ export const TariffSection = () => {
         className={s.sun}
       />
 
-      <Image
-        alt="svg"
-        width={1920}
-        height={1080}
-        priority
-        src="/icons/noname.svg"
-        className={s.svg}
-      />
+      {noName}
     </section>
   );
 };
 
 const line = (
-  <svg viewBox="0 0 215 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M1.5 16C1.99504 12.7275 10.5509 10.5471 13.6202 9.70162C20.4333 7.82501 25.6192 4.84878 32.9877 4.84878C37.4642 4.84878 56.3696 1.53741 46.7323 7.94864C44.249 9.60068 44.2465 8.67763 42.964 10.5323C41.2072 13.0726 44.3939 11.2041 46.4001 10.9969C56.3011 9.97419 64.8251 10.3024 74.8463 9.60068C91.0668 8.46477 109.21 10.7211 125.208 8.2564C137.194 6.40974 149.744 6.55482 161.674 4.84869C169.578 3.71824 177.55 2.26739 185.602 2.26739C188.261 2.26739 194.761 0.96031 194.536 4.12593C194.413 5.85198 187.875 8.12105 186.101 8.77227C184.146 9.49027 173.901 12.4893 177.667 12.4893C194.581 12.4893 196.933 12.2548 213.5 9.60068"
+  <motion.svg
+    viewBox="0 0 215 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{verticalAlign: "middle" }}
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <motion.path
+      d="M2.00004 20.7578C2.45188 17.5375 10.2611 15.3919 13.0626 14.56C19.2811 12.7133 24.0145 9.7846 30.74 9.7846C34.8258 9.7846 52.0815 6.52609 43.2852 12.835C41.0185 14.4606 41.0163 13.5523 39.8457 15.3773C38.2423 17.8772 41.1509 16.0385 42.982 15.8346C52.0189 14.8282 59.7991 15.1512 68.9458 14.4606C83.7509 13.3429 100.311 15.5632 114.913 13.1378C125.853 11.3206 137.308 11.4634 148.196 9.78451C155.411 8.67211 162.687 7.24442 170.036 7.24442C172.464 7.24442 178.396 5.9582 178.191 9.07329C178.078 10.7718 172.111 13.0046 170.492 13.6455C168.708 14.352 159.357 17.3032 162.794 17.3032C178.232 17.3032 192.608 14.4284 207.729 11.8166C220.689 9.57803 233.794 5.41555 246.961 5.41555C262.394 5.41555 277.11 3.58668 292.637 3.58668C298.544 3.58668 303.415 1.68424 303.415 7.70164C303.415 9.69628 303.166 11.8532 302.16 13.6455C301.673 14.513 297.455 18.4038 298.282 16.7444C300.841 11.6149 311.575 11.429 316.758 9.78451C324.582 7.30183 333.611 5.67529 341.848 4.50112C349.491 3.41176 357.215 1.75781 365 1.75781"
       stroke="#FFF9C1"
       strokeWidth="3"
       strokeLinecap="round"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{ duration: 1.2, ease: "easeOut" }}
+      viewport={{ once: false, amount: 0.5 }}
     />
-  </svg>
+  </motion.svg>
 );
 
-// export const noName = (
-//   <motion.svg className={s.svg} viewBox="0 0 130 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <motion.path
-//       d="M125.322 84.5919C122.199 84.0242 119.911 80.145 117.318 78.474C114.823 76.8658 112.353 74.3433 110.425 72.1047C102.962 63.4372 94.1776 56.0462 86.3935 47.6331C82.8291 43.7806 79.5364 40.9166 77.2376 36.3191C76.2897 34.4233 75.1599 32.1672 73.6549 30.6622C73.0146 30.0219 72.2058 28.973 72.1464 28.0222C72.1251 27.6823 72.1464 29.3387 72.1464 29.7193C72.1464 31.1527 73.104 32.5798 73.2777 34.0563C73.4688 35.6804 74.0486 37.808 74.4301 39.5247C74.8915 41.6013 74.7863 43.758 74.7863 45.936C74.7863 54.8765 75.2821 63.9752 74.7863 72.9008C74.6178 75.9342 72.8821 79.6366 71.6854 82.3291C70.3043 85.4366 67.8642 88.4969 65.5256 90.8355C62.6044 93.7567 60.296 96.6201 56.4745 98.3572C53.1121 99.8855 48.9123 101.94 45.1815 101.94C41.6083 101.94 38.4065 101.411 34.978 100.788C33.0188 100.431 31.4505 99.2167 29.9077 98.2524C28.6475 97.4648 29.1534 94.9417 29.1534 93.643C29.1534 92.0727 30.3778 91.3802 31.7933 91.3802C33.4216 91.3802 34.7292 94.0667 34.8104 95.5287C35.0549 99.9294 32.1582 101.726 29.7191 104.978C27.8721 107.441 25.6406 109.476 23.8736 112.039C22.0651 114.661 20.1568 115.728 17.2738 116.837C12.6946 118.598 6.83346 117.779 2 117.779"
-//       stroke="#FFF9C1"
-//       stroke-width="3"
-//       stroke-linecap="round"
-//       pathLength={1}
-//       initial={{ pathLength: 0 }}
-//       whileInView={{ pathLength: 1 }}
-//       transition={{
-//         duration: 1.2,
-//         ease: "easeOut",
-//         repeat: Infinity,
-//         repeatType: "reverse",
-//         repeatDelay: 2,
-//       }}
-//       viewport={{ once: false, amount: 0.5 }}
-//     />
-//     <motion.path
-//       d="M91.7578 75.541C91.7578 77.1199 93.0161 78.3078 94.1044 79.3961C95.7558 81.0475 97.6631 82.5854 99.489 84.0055C100.517 84.8052 101.545 85.7824 102.883 86.0797C104.711 86.4859 106.609 87.0941 107.597 88.7406"
-//       stroke="#FFF9C1"
-//       stroke-width="3"
-//       stroke-linecap="round"
-//       pathLength={1}
-//       initial={{ pathLength: 0 }}
-//       whileInView={{ pathLength: 1 }}
-//       transition={{
-//         duration: 1.2,
-//         ease: "easeOut",
-//         repeat: Infinity,
-//         repeatType: "reverse",
-//         repeatDelay: 2,
-//       }}
-//       viewport={{ once: false, amount: 0.5 }}
-//     />
-//     <motion.path
-//       d="M94.0195 31.416C99.4114 31.416 104.173 34.8102 109.482 34.8102"
-//       stroke="#FFF9C1"
-//       stroke-width="3"
-//       stroke-linecap="round"
-//       pathLength={1}
-//       initial={{ pathLength: 0 }}
-//       whileInView={{ pathLength: 1 }}
-//       transition={{
-//         duration: 1.2,
-//         ease: "easeOut",
-//         repeat: Infinity,
-//         repeatType: "reverse",
-//         repeatDelay: 2,
-//       }}
-//       viewport={{ once: false, amount: 0.5 }}
-//     />
-//     <motion.path
-//       d="M91.3789 13.3139C99.3447 13.3139 106.622 11.8954 113.839 8.41123C116.628 7.06484 119.566 5.9247 122.283 4.4304C123.897 3.5423 126.057 2 127.961 2"
-//       stroke="#FFF9C1"
-//       stroke-width="3"
-//       stroke-linecap="round"
-//       pathLength={1}
-//       initial={{ pathLength: 0 }}
-//       whileInView={{ pathLength: 1 }}
-//       transition={{
-//         duration: 1.2,
-//         ease: "easeOut",
-//         repeat: Infinity,
-//         repeatType: "reverse",
-//         repeatDelay: 2,
-//       }}
-//       viewport={{ once: false, amount: 0.5 }}
-//     />
-//   </motion.svg>
-// );
+const noName = (
+  <motion.svg
+    className={s.svg}
+    viewBox="0 0 130 120"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ verticalAlign: "middle" }}
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <motion.path
+      d="M125.322 84.5919C122.199 84.0242 119.911 80.145 117.318 78.474C114.823 76.8658 112.353 74.3433 110.425 72.1047C102.962 63.4372 94.1776 56.0462 86.3935 47.6331C82.8291 43.7806 79.5364 40.9166 77.2376 36.3191C76.2897 34.4233 75.1599 32.1672 73.6549 30.6622C73.0146 30.0219 72.2058 28.973 72.1464 28.0222C72.1251 27.6823 72.1464 29.3387 72.1464 29.7193C72.1464 31.1527 73.104 32.5798 73.2777 34.0563C73.4688 35.6804 74.0486 37.808 74.4301 39.5247C74.8915 41.6013 74.7863 43.758 74.7863 45.936C74.7863 54.8765 75.2821 63.9752 74.7863 72.9008C74.6178 75.9342 72.8821 79.6366 71.6854 82.3291C70.3043 85.4366 67.8642 88.4969 65.5256 90.8355C62.6044 93.7567 60.296 96.6201 56.4745 98.3572C53.1121 99.8855 48.9123 101.94 45.1815 101.94C41.6083 101.94 38.4065 101.411 34.978 100.788C33.0188 100.431 31.4505 99.2167 29.9077 98.2524C28.6475 97.4648 29.1534 94.9417 29.1534 93.643C29.1534 92.0727 30.3778 91.3802 31.7933 91.3802C33.4216 91.3802 34.7292 94.0667 34.8104 95.5287C35.0549 99.9294 32.1582 101.726 29.7191 104.978C27.8721 107.441 25.6406 109.476 23.8736 112.039C22.0651 114.661 20.1568 115.728 17.2738 116.837C12.6946 118.598 6.83346 117.779 2 117.779"
+      stroke="#FFF9C1"
+      strokeWidth="3"
+      strokeLinecap="butt"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{
+        duration: 1.2,
+        ease: "easeOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 2,
+      }}
+      viewport={{ once: false, amount: 0.5 }}
+    />
+    <motion.path
+      d="M91.7578 75.541C91.7578 77.1199 93.0161 78.3078 94.1044 79.3961C95.7558 81.0475 97.6631 82.5854 99.489 84.0055C100.517 84.8052 101.545 85.7824 102.883 86.0797C104.711 86.4859 106.609 87.0941 107.597 88.7406"
+      stroke="#FFF9C1"
+      strokeWidth="3"
+      strokeLinecap="butt"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{
+        duration: 1.2,
+        ease: "easeOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 2,
+      }}
+      viewport={{ once: false, amount: 0.5 }}
+    />
+    <motion.path
+      d="M94.0195 31.416C99.4114 31.416 104.173 34.8102 109.482 34.8102"
+      stroke="#FFF9C1"
+      strokeWidth="3"
+      strokeLinecap="butt"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{
+        duration: 1.2,
+        ease: "easeOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 2,
+      }}
+      viewport={{ once: false, amount: 0.5 }}
+    />
+    <motion.path
+      d="M91.3789 13.3139C99.3447 13.3139 106.622 11.8954 113.839 8.41123C116.628 7.06484 119.566 5.9247 122.283 4.4304C123.897 3.5423 126.057 2 127.961 2"
+      stroke="#FFF9C1"
+      strokeWidth="3"
+      strokeLinecap="butt"
+      pathLength={1}
+      initial={{ pathLength: 0 }}
+      whileInView={{ pathLength: 1 }}
+      transition={{
+        duration: 1.2,
+        ease: "easeOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+        repeatDelay: 2,
+      }}
+      viewport={{ once: false, amount: 0.5 }}
+    />
+  </motion.svg>
+);
 
 const svg = (
   <svg
