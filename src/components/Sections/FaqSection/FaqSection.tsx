2999244bdafdeb33b98cc8e284b3b39f97d2cbb7
klimisnotavailable <klimgrigor28@gmail.com>
2025-06-18 19:47:25 +0300

    Update FaqSection.tsx

--- src/components/Sections/FaqSection/FaqSection.tsx
+++ src/components/Sections/FaqSection/FaqSection.tsx
@@ -7,7 +7,7 @@ import { useEffect, useMemo, useState } from "react";
 import { API_URL } from "@/constants";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { AnimatedLine } from "@/components/AnimatedLine/AnimatedLine";
 
 type FaqItem = {
@@ -146,30 +146,30 @@ export const FaqSection = ({
   );
 };
 
-const Line = () => {
-  return (
-    <motion.svg
-      viewBox="0 0 245 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={{
-        width: "100%",
-        height: "auto",
-        verticalAlign: "middle",
-      }}
-      preserveAspectRatio="xMidYMid meet"
-    >
-      <motion.path
-        d="M1.5 15.5938C2.06509 12.3212 11.8317 10.1408 15.3354 9.29538C23.1125 7.41876 29.0323 4.44253 37.4435 4.44253C42.5535 4.44253 64.1342 1.13116 53.1331 7.5424C50.2983 9.19443 50.2955 8.27138 48.8315 10.126C46.8262 12.6664 50.4638 10.7979 52.7539 10.5906C64.0559 9.56794 73.7862 9.8962 85.2254 9.19443C103.741 8.05852 124.452 10.3149 142.714 7.85015C156.396 6.00349 170.722 6.14857 184.34 4.44244C193.362 3.31199 202.462 1.86114 211.654 1.86114C214.69 1.86114 222.109 0.554059 221.852 3.71968C221.712 5.44573 214.248 7.7148 212.224 8.36602C209.993 9.08402 198.298 12.0831 202.597 12.0831C221.904 12.0831 224.589 11.8486 243.5 9.19443"
-        stroke="#FF91B2"
-        strokeWidth="3"
-        strokeLinecap="round"
-        pathLength={1}
-        initial={{ pathLength: 0 }}
-        whileInView={{ pathLength: 1 }}
-        transition={{ duration: 1.2, ease: "easeOut" }}
-        viewport={{ once: false, amount: 0.5 }}
-      />
-    </motion.svg>
-  );
-};
+// const Line = () => {
+//   return (
+//     <motion.svg
+//       viewBox="0 0 245 18"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//       style={{
+//         width: "100%",
+//         height: "auto",
+//         verticalAlign: "middle",
+//       }}
+//       preserveAspectRatio="xMidYMid meet"
+//     >
+//       <motion.path
+//         d="M1.5 15.5938C2.06509 12.3212 11.8317 10.1408 15.3354 9.29538C23.1125 7.41876 29.0323 4.44253 37.4435 4.44253C42.5535 4.44253 64.1342 1.13116 53.1331 7.5424C50.2983 9.19443 50.2955 8.27138 48.8315 10.126C46.8262 12.6664 50.4638 10.7979 52.7539 10.5906C64.0559 9.56794 73.7862 9.8962 85.2254 9.19443C103.741 8.05852 124.452 10.3149 142.714 7.85015C156.396 6.00349 170.722 6.14857 184.34 4.44244C193.362 3.31199 202.462 1.86114 211.654 1.86114C214.69 1.86114 222.109 0.554059 221.852 3.71968C221.712 5.44573 214.248 7.7148 212.224 8.36602C209.993 9.08402 198.298 12.0831 202.597 12.0831C221.904 12.0831 224.589 11.8486 243.5 9.19443"
+//         stroke="#FF91B2"
+//         strokeWidth="3"
+//         strokeLinecap="round"
+//         pathLength={1}
+//         initial={{ pathLength: 0 }}
+//         whileInView={{ pathLength: 1 }}
+//         transition={{ duration: 1.2, ease: "easeOut" }}
+//         viewport={{ once: false, amount: 0.5 }}
+//       />
+//     </motion.svg>
+//   );
+// };
