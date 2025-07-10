4e375d37e6c3f9de4cef50756ad48acfa5489b53
klimisnotavailable <klimgrigor28@gmail.com>
2025-07-10 19:24:00 +0300

    Update SidePanel.jsx

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -96,7 +96,7 @@ const SidePanel = ({ isOpen, onClose, locale }) => {
             </li>
 
             <li>
-              <Link href="viber://chat?number=+380508812637">
+              <Link href="viber://chat?number=+380983085847">
                 <Image
                   width={32}
                   height={32}
